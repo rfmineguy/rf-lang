627b11399e68cfb63525e77f4b53081aef681708
rfmineguy <rfmineguy@gmail.com>
2022-06-21 22:54:23 -0700

    Continued word on parser and explored various approaches, but I think the one to stick with is to use regex on the lex file

--- x86_64/compiler/src/parser.cpp
+++ x86_64/compiler/src/parser.cpp
@@ -1,6 +1,8 @@
 #include "parser.h"
+#include <cstddef>
 #include <sstream>
 #include <algorithm>
+#include <regex>
 
 Parser::Parser(const std::string& lexFilePath, const std::string& sourceFilePath)
 :currentLineNum(1) {
@@ -94,9 +96,75 @@ std::vector<std::string> Parser::SplitLine(const std::string& line) {
 }
 
 Result Parser::CheckLineSyntax(const std::vector<std::string>& words, const std::string& sourceLine, int lnum) {
-    printf("Checking [%3d] : %s\n", lnum, sourceLine.c_str());
+    //printf("Checking [%3d] : %s\n", lnum, sourceLine.c_str());
     //PrintStrVec(words);
-    //printf("\n");
+    std::string lexString;
+    for (const std::string& s : words) {
+        lexString += s;
+        lexString += " ";
+    }
+    std::regex function("^(FUNC).*(ARROW TYPE)");
+    std::regex argList("(OPEN_PAREN).*(CLOSE_PAREN)");
+    std::regex ret("^(RET).*(SEMI)");
+    std::regex closeBracket("CLOSE_BRACE");
+    std::smatch matches;
+
+    bool isFunctionDecl = false;
+    bool isFunctionCall = false;
+    bool isReturnStatement = false;
+
+    //if (std::regex_search(lexString, matches, argList)) {
+    //    if (std::regex_search(lexString, matches, function)) {
+    //        isFunctionDecl = true;
+    //    }
+    //    else {
+    //        isFunctionCall = true;
+    //    }
+    //}
+    //else if (std::regex_search(lexString, matches, ret)) {
+    //    isReturnStatement = true;
+    //}
+
+    if (std::regex_search(lexString, matches, argList)) {
+        //printf("Found arglist, %d\n", lnum);
+        size_t openParenPos = sourceLine.find('(');
+        size_t closeParenPos = sourceLine.find(')');
+        size_t length = closeParenPos - openParenPos;
+
+        if (std::regex_search(lexString, matches, function)) {
+            int len = openParenPos - 4;
+            size_t arrowPos = sourceLine.find("->");
+
+            std::string funcName = sourceLine.substr(4, len);
+            std::string params = sourceLine.substr(openParenPos + 1, length - 1);
+            std::string retType = sourceLine.substr(arrowPos + 3, 5);
+            printf("Found function, line=%d, name=%s, params=\"%s\", return=%s\n", lnum, funcName.c_str(), params.c_str(), retType.c_str());
+            return Result(false, "Function decl", "Nothing wrong", lnum, sourceLine);
+        }
+        else {
+            //printf("Found function call\n");
+            int len = openParenPos;
+            
+            std::string funcName = sourceLine.substr(0, len);
+            std::string arguments = sourceLine.substr(openParenPos + 1, length - 1);
+            printf("Found function call, line=%d, name=%s, params=\"%s\"\n", lnum, funcName.c_str(), arguments.c_str());
+            return Result(false, "Function call", "Nothing wrong", lnum, sourceLine);
+        }
+    }
+    else if (std::regex_search(lexString, matches, ret)) {
+        size_t semiPos = sourceLine.find(";");
+        std::string id = sourceLine.substr(4, semiPos - 4);
+        printf("Found return statement, line=%d, value=%s\n", lnum, id.c_str());
+        return Result(false, "Return statement", "Nothing wrong", lnum, sourceLine);
+    }
+    else if (std::regex_search(lexString, matches, closeBracket)) {
+        //write stackframe finish code
+    }
+    else {
+        printf("Statement not supported yet, line=%d\n", lnum);
+    }
+
+    /*
     if (words[0] == "FUNC") {
         if (words[1] != "ID")
             return Result(true, "Function decl", "Missing function name", lnum, sourceLine);
@@ -131,8 +199,19 @@ Result Parser::CheckLineSyntax(const std::vector<std::string>& words, const std:
     else if (words[0] == "ID") {
         if (words[1] != "ASSIGN" || (words[1] != "PLUS" && words[2] != "PLUS") || (words[1] != "MINUS" && words[2] != "MINUS"))
             return Result(true, "Assignment", "Attempting to use id without assignment", lnum, sourceLine);
-        
+        if (words[1] == "OPEN_PAREN") {
+
+        }
+        //if (words[1] == "ASSIGN") {
+        //    if (words[2] == "ID") {
+
+        //    }
+        //    else if (words[2] == "SLITERAL" || words[2] == "BLITERAL" || words[2] == "HLITERAL") {
+        //    
+        //    }
+        //}
         return Result(false, "Assignment", "No errors", lnum, sourceLine);
     }
     return Result(false, "Unknown", "Feature encountered that's not implemented", lnum, sourceLine);
+    */
 }
